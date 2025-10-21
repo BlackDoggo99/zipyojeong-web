5bb6104b829823ef39292fbe9ae0e35483a4fc30
BlackDoggo99 <dlwjdrhks8@gmail.com>
2025-10-22 08:54:41 +0900

    Create _document.tsx
    google search console 인증을 위한 document 파일 생성

---
+++ src/app/_document.tsx
@@ -0,0 +1,16 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+       <meta name="google-site-verification" content="QLrbGyZfF4dcpBGkq7LqXO6ylYLI1oCOO3vXRvGY_CY" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
