2b3eec68ce16d808b2d02a6a75465c461e005180
이정관 <dlwjdrhks8@gmail.com>
2025-10-25 05:12:31 +0900

    결제완료창 수정

--- src/app/checkout/complete/page.tsx
+++ src/app/checkout/complete/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+"use client";  // ✅ 꼭 최상단에
+export const dynamic = "force-dynamic";
 
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
